77d37ee213dc06f983250845e869f7e725ad35d2
dingirtorres <dingirtorres3@gmail.com>
2024-09-11 23:44:21 -0300

    Add files via upload

---
+++ 5/js/js.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const botonComprar = document.querySelector('.comprar');
+
+
+    botonComprar.addEventListener('click', function() {
+        
+        alert('whatsapp 3416405962');
+    });
+});
